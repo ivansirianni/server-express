af11d9148516f37d1077717f496d9e8bcd8c84b8
ivansirianni <ivansirianni@gmail.com>
2024-05-23 17:59:09 -0300

    probando server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const { envs } = require('./config/env')
+const { startServer } = require('./server/server')
+
+const main = () =>{
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    })
+}
+
+//funcion agnostica autoconvocada
+//*Agnostica por que no tiene nombre
+//*Autoconvocada por que se ejecuta finalizando con ()
+(async() => {
+    main()
+})()
---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const startServer = (options) =>{
+    const {port, public_path = 'public'} = options
+    const app = express()
+    //para poder usar middlewares se usa la palabra use (propio de express)
+    app.use(express.static(public_path)) // toda la info estatica
+    app.get('*', (req, res)=>{
+        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+
+    app.listen(port, () =>{
+        console.log(`Escuchando en el puerto ${port}`)
+    })
+}
+
+module.exports = {
+    startServer
+}
